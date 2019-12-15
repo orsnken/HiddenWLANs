e5acba2c33e6caa193f72f5c9d9b9c29e2c939fc
orsnken <hiroshima@net.itc.nagoya-u.ac.jp>
2019-12-15 16:58:56 +0900

    commit

---
+++ __src__/heatmap.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("output/D30_throuput.csv", sep=",")
+df.head(20)
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# export PATH="/home/ken/App/ns-3.29/source/ns-3.29:$PATH"
-export PATH="/home/hiroshima-lenovo/App/ns3/source/ns-3.29:$PATH"
+export PATH="/home/ken/App/ns-3.29/source/ns-3.29:$PATH"
+# export PATH="/home/hiroshima-lenovo/App/ns3/source/ns-3.29:$PATH"
 
 waf
 
@@ -17,7 +17,7 @@ echo "Begin simulation ///"
 pwd
 
 sim_time=3
-distance_disturbers=50
+distance_disturbers=100
 
 for y in -100 -90 -80 -70 -60 -50 -40 -30 -20 -10 0 10 20 30 40 50 60 70 80 90 100
 do
