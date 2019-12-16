64f354da65c5ca205a1695d75c978b69476df9e6
orsnken <hiroshima@net.itc.nagoya-u.ac.jp>
2019-12-16 16:46:20 +0900

    modify heatmap.py

--- __src__/heatmap.py
+++ __src__/heatmap.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("output/D30_throuput.csv", sep=",")
-df.head(20)
+df = pd.read_csv("output/D30_throughput.csv", index_col=0)
+print(df.dropna(how="all", axis=1, inplace=True))
+
+plt.figure()
+sns.heatmap(df, cmap="Spectral_r")
+plt.show()
+plt.close("all")
